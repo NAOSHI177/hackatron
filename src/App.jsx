97669d4b034363ae63393cd6790e13ff3f47b8de
naoshi17 <naoshi.arguello7@gmail.com>
2024-10-05 12:58:39 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './assets/components/Header'
+import Main from './assets/components/Main'
+import Footer from './assets/components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
